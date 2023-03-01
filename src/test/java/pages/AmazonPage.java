8f18db846a388371c63eacbbf4c47445c1de860c
ahmet bulutluoz <abbulutluoz@gmail.com>
2023-03-01 09:50:25 +0100

    11.gun sonu

--- src/test/java/pages/AmazonPage.java
+++ src/test/java/pages/AmazonPage.java
@@ -27,7 +27,7 @@ public class AmazonPage {
     }
 
     @FindBy(id="twotabsearchtextbox")
-    public WebElement aramaKutusu;
+    public static WebElement aramaKutusu;
 
     @FindBy(xpath = "//h1[@class='a-size-base s-desktop-toolbar a-text-normal']")
     public WebElement aramaSonucElementi;
---
+++ src/test/java/tests/day11_POM_assertions/C01_configReaderKullanimi.java
@@ -0,0 +1,32 @@
+package tests.day11_POM_assertions;
+
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.AmazonPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class C01_configReaderKullanimi {
+
+    @Test
+    public void test01(){
+
+        // amazon anasayfaya gidelim
+        Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+
+        // Test datasi olarak verilen kelime icin arama yapalim
+        AmazonPage amazonPage = new AmazonPage();
+        AmazonPage.aramaKutusu.sendKeys(ConfigReader.getProperty("amazonAranacakKelime")+ Keys.ENTER);
+
+        // arama sonuclarinin arattigimiz test datasini icerdigini test edelim
+
+        String expectedIcerik = ConfigReader.getProperty("amazonAranacakKelime");
+        String actualAramaSonucYazisi= amazonPage.aramaSonucElementi.getText();
+
+        Assert.assertTrue(actualAramaSonucYazisi.contains(expectedIcerik));
+
+        Driver.closeDriver();
+
+    }
+}
